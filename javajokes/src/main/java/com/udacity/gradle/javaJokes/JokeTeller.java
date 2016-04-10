b919724d1cbf6aa8b80466a118529c106e99a065
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-04-10 18:50:00 +0200

    Return a joke from the javajokes library

---
+++ javajokes/src/main/java/com/udacity/gradle/javaJokes/JokeTeller.java
@@ -0,0 +1,8 @@
+package com.udacity.gradle.javaJokes;
+
+public class JokeTeller {
+
+    public String tellMeAJoke() {
+        return "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+    }
+}
