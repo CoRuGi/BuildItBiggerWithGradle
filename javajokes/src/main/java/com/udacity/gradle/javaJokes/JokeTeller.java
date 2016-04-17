3a837a6f7141a1f35a2a59c58df7247595099657
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-04-17 15:15:33 +0200

    Escape apostrophe in java string

--- javajokes/src/main/java/com/udacity/gradle/javaJokes/JokeTeller.java
+++ javajokes/src/main/java/com/udacity/gradle/javaJokes/JokeTeller.java
@@ -3,6 +3,6 @@ package com.udacity.gradle.javaJokes;
 public class JokeTeller {
 
     public String tellMeAJoke() {
-        return "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.";
+        return "Can a kangaroo jump higher than a house? Of course, a house doesn\'t jump at all.";
     }
 }
