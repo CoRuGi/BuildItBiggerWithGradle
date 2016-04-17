50e46c786123d4a89f3870059ddeccf5f11556a0
CoRuGi <CR.Github.n6qc4@gmail.com>
2016-04-17 18:34:14 +0200

    Process the intent when creating the JokePresenter Activity.

--- jokePresenter/src/main/java/com/androidnanodegree/cr/jokepresenter/JokePresenter.java
+++ jokePresenter/src/main/java/com/androidnanodegree/cr/jokepresenter/JokePresenter.java
@@ -1,13 +1,14 @@
 package com.androidnanodegree.cr.jokepresenter;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.TextView;
 
 public class JokePresenter extends AppCompatActivity {
+
+    private static final String newJoke = "com.androidnanodegree.cr.NEW_JOKE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +17,15 @@ public class JokePresenter extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        Intent intent = getIntent();
+        TextView jokeTextView = (TextView) findViewById(R.id.joke_text_view);
+        String joke = intent.getStringExtra(newJoke);
+        if (joke == null) {
+            joke = "An unfortunate error occurred. So apparently the joke is on the developer :)";
+        }
+        if (jokeTextView != null) {
+            jokeTextView.setText(joke);
+        }
     }
 
 }
